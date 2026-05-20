2389ab7a0bcb80a56d68491e53093d37a23e89bb
myakayakal01 <32677433+myakayakal01@users.noreply.github.com>
2026-05-20 14:43:01 +0200

    Modified the Homepage

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -1,10 +1,21 @@
 package Pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 public class HomePage {
+
+
+    WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
 
     @FindBy(id = "add-to-cart-sauce-labs-backpack")
     WebElement backpack;
@@ -24,6 +35,16 @@ public class HomePage {
 
     @FindBy(id = "add-to-cart-test.allthethings()-t-shirt-(red)")
     WebElement shirt;
+
+    @FindBy(xpath = "//*[@id=\"header_container\"]/div[2]/span")
+    WebElement productText;
+
+
+    public void verifyProductsPage() {
+        String actualText = productText.getText();
+        Assert.assertEquals(actualText, "Products");
+    }
+
 
     public void addBackpackToCart() {
         backpack.click();
@@ -47,5 +68,9 @@ public class HomePage {
 
     public void addShirtToCart() {
         shirt.click();
+    }
+
+    public void setBackpack(WebElement backpack) {
+        this.backpack = backpack;
     }
 }
